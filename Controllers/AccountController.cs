89587b17cb59ecc7af8848e57fb07f656b8b2935
fraeron <rafaeljtenido@gmail.com>
2024-10-15 19:19:01 +0800

    Add files of recreation

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RE_HUB.Models;
+using System.Text;
+
+namespace RE_HUB.Controllers
+{
+    public class AccountController : Controller
+    {
+        private string ApiUrl = "https://localhost:7222/api/user";
+
+        // GET: AccountController
+        public async Task<ActionResult> Index()
+        {
+            List<User> users = new List<User>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(ApiUrl);
+
+                var result = await response.Content.ReadAsStringAsync();
+                users = JsonConvert.DeserializeObject<List<User>>(result);
+            }
+
+            return View(users);
+        }
+
+        // GET: AccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(User user)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(ApiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(user);
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RE_HUB.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string status { get; set; }
+    }
+}
